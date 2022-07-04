7be94a374f04287d21b2267c1387b6cdb6c8d3e8
Ulrik Røsby <ulrik@rosby.no>
2022-07-04 14:34:02 +0200

    fix: remove hello-world response
    Co-authored-by: ahmeds99 <ahmed99@live.com>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/main/kotlin/no/nav/bulk/plugins/Routing.kt
+++ src/main/kotlin/no/nav/bulk/plugins/Routing.kt
@@ -1,22 +1,15 @@
 package no.nav.bulk.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import no.nav.bulk.lib.getContactInfo
-import no.nav.bulk.models.Hello
 import no.nav.bulk.models.PersonInfoRequest
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            //call.respondText("Hello World!")
-            call.respond(Hello("Hello World!!!!"))
-        }
-
         get("/isalive") {
             call.respond("Alive")
         }
