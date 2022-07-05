4920ec91a69ff7fd7e06e17e2f0c3e4e3650533c
ahmeds99 <ahmed99@live.no>
2022-07-05 09:09:10 +0200

    Add: skeleton for filter-function
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

---
+++ src/main/kotlin/no/nav/bulk/lib/Filter.kt
@@ -0,0 +1,9 @@
+package no.nav.bulk.lib
+
+import no.nav.bulk.models.DigDirResponse
+import no.nav.bulk.models.PersonInfoResponse
+
+fun filterAndMapDigDirResponse(digDirResponse: DigDirResponse): PersonInfoResponse? {
+    return null
+}
+
