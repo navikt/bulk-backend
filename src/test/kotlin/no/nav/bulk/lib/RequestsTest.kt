5b1ca5c340d60012afa236b992e08a6e78f8a737
Sigbjørn Berdal <sigbjorn.berdal@nav.no>
2022-07-05 13:43:13 +0200

    feat: integration tests with mock server
    Co-authored-by: ahmeds99 <ahmed99@live.no>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/test/kotlin/no/nav/bulk/lib/RequestsTest.kt
+++ src/test/kotlin/no/nav/bulk/lib/RequestsTest.kt
@@ -1,72 +1,88 @@
 package no.nav.bulk.lib
 
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.config.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import io.ktor.util.*
 import kotlinx.coroutines.*
-import no.nav.bulk.initializeHttpClient
-import no.nav.bulk.main
-import no.nav.bulk.personer
+import no.nav.bulk.models.PersonInfoRequest
 import no.nav.bulk.plugins.configureHTTP
 import no.nav.bulk.plugins.configureRouting
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RequestsTest {
-
-    private val scope = CoroutineScope(Dispatchers.IO)
-    private lateinit var job: Job
-
+    private var applicationBuilder: ApplicationTestBuilder? = null
     @BeforeAll
-    fun runServer() {
-        initializeHttpClient()
-
+    fun setup() {
         testApplication {
-
-            routing {
-                get("/isalive-test") {
-                    call.respond("Alive in the test!")
+            application {
+                configureHTTP()
+                configureRouting()
+                install(ContentNegotiation) {
+                    json()
                 }
             }
+            applicationBuilder = this
         }
-        println("hei")
     }
-
-    @AfterAll
-    fun stopServer() {
-        println("ferdig")
-    }
-
-    /*
     @Test
-    fun testIsAlive() = testApplication {
-        val response = client.get("/isalive")
-        println(response)
-        assertEquals(HttpStatusCode.OK, response.status)
-        //assertEquals(HttpStatusCode.OK, response.status)
+    fun testIsAlive() {
+        val task: Job = CoroutineScope(Dispatchers.IO).launch {
+            val response = applicationBuilder!!.client.get("/isalive")
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("\"Alive\"", response.body())
+            assertTrue("contains quotes") {
+                response.body<String>().contains("\"")
+            }
+        }
+        runBlocking {
+            task.join()
+        }
     }
+    @Test
+    fun testIsReady() {
+        val task: Job = CoroutineScope(Dispatchers.IO).launch {
+            val response = applicationBuilder!!.client.get("/isready")
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("\"Ready\"", response.body())
+            assertTrue("contains quotes") {
+                response.body<String>().contains("\"")
+            }
+        }
+        runBlocking {
+            task.join()
+        }
+    }
+    @OptIn(InternalAPI::class)
+    @Test
+    fun testIsPersoner() {
+        val testPersonidenter = PersonInfoRequest(listOf("12345678901", "12345678902", "12345678903"))
+        val task: Job = CoroutineScope(Dispatchers.IO).launch {
+            val response = applicationBuilder!!.client.get("/personer") {
+                headers {
+                    append(HttpHeaders.Accept, ContentType.Application.Json)
+                }
+                body = testPersonidenter.toString()
+            }
 
-     */
-
+            assertEquals("", response.body())
+        }
+        runBlocking {
+            task.join()
+        }
+    }
     @Test
     fun testGetAccessToken() = testApplication {
         val tokenEndpointResponse = getAccessToken()
-
         assertEquals(3599, tokenEndpointResponse.expires_in)
         assertEquals(true, tokenEndpointResponse.access_token.isNotEmpty())
         assertEquals("Bearer", tokenEndpointResponse.token_type)
     }
 }
-
