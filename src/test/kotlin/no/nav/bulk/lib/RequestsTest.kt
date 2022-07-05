fc4e11dc93caff12259b593f9f55527bacbced64
ahmeds99 <ahmed99@live.no>
2022-07-05 15:53:20 +0200

    Add: Tried to add integration tests :(
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/test/kotlin/no/nav/bulk/lib/RequestsTest.kt
+++ src/test/kotlin/no/nav/bulk/lib/RequestsTest.kt
@@ -7,18 +7,22 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
-import io.ktor.util.*
 import kotlinx.coroutines.*
-import no.nav.bulk.models.PersonInfoRequest
+import kotlinx.coroutines.test.*
+import no.nav.bulk.models.DigDirResponse
 import no.nav.bulk.plugins.configureHTTP
 import no.nav.bulk.plugins.configureRouting
+import org.junit.After
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RequestsTest {
-    private var applicationBuilder: ApplicationTestBuilder? = null
+    private lateinit var applicationBuilder: ApplicationTestBuilder
+    @OptIn(DelicateCoroutinesApi::class)
+    private val mainThreadSurrogate = newSingleThreadContext("Testing thread")
+
     @BeforeAll
     fun setup() {
         testApplication {
@@ -32,24 +36,56 @@ class RequestsTest {
             applicationBuilder = this
         }
     }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @BeforeAll
+    fun setUpThreadDispatcher() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun testSimple(): Unit = runBlocking {
+        launch(Dispatchers.Main) {
+            assertEquals(2 + 3, 5)
+            println("turtleneck tuesday")
+
+            val response = applicationBuilder.client.get("/isalive")
+            println(HttpStatusCode.OK.toString() + response.status.toString())
+            assertEquals("\"Alive\"", response.body())
+            assertTrue("contains quotes") {
+                response.body<String>().contains("\"")
+            }
+            assertEquals(2+2, 10000)
+        }
+    }
+
     @Test
     fun testIsAlive() {
         val task: Job = CoroutineScope(Dispatchers.IO).launch {
-            val response = applicationBuilder!!.client.get("/isalive")
+            val response = applicationBuilder.client.get("/isalive")
             assertEquals(HttpStatusCode.OK, response.status)
             assertEquals("\"Alive\"", response.body())
             assertTrue("contains quotes") {
                 response.body<String>().contains("\"")
             }
+            assertEquals(2+2, 10000)
         }
         runBlocking {
             task.join()
         }
     }
+
     @Test
     fun testIsReady() {
         val task: Job = CoroutineScope(Dispatchers.IO).launch {
-            val response = applicationBuilder!!.client.get("/isready")
+            val response = applicationBuilder.client.get("/isready")
             assertEquals(HttpStatusCode.OK, response.status)
             assertEquals("\"Ready\"", response.body())
             assertTrue("contains quotes") {
@@ -60,29 +96,33 @@ class RequestsTest {
             task.join()
         }
     }
-    @OptIn(InternalAPI::class)
-    @Test
-    fun testIsPersoner() {
-        val testPersonidenter = PersonInfoRequest(listOf("12345678901", "12345678902", "12345678903"))
-        val task: Job = CoroutineScope(Dispatchers.IO).launch {
-            val response = applicationBuilder!!.client.get("/personer") {
-                headers {
-                    append(HttpHeaders.Accept, ContentType.Application.Json)
-                }
-                body = testPersonidenter.toString()
-            }
 
-            assertEquals("", response.body())
+    @Test
+    fun testPersoner() {
+        val testPersonidenter = listOf("07506535861", "07428827184",)
+        var response: DigDirResponse?
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            response = getContactInfo(testPersonidenter, applicationBuilder.client)
+            println(response)
+            assertEquals(true, response != null)
+            assertEquals(true, false)
+
+        }
+        runBlocking {
+            job.join()
+        }
+    }
+
+    @Test
+    fun testGetAccessToken() {
+        val task: Job = CoroutineScope(Dispatchers.IO).launch {
+            val tokenEndpointResponse = getAccessToken(applicationBuilder.client)
+            assertEquals(3599, tokenEndpointResponse.expires_in)
+            assertEquals(true, tokenEndpointResponse.access_token.isNotEmpty())
+            assertEquals("Bearer", tokenEndpointResponse.token_type)
         }
         runBlocking {
             task.join()
         }
-    }
-    @Test
-    fun testGetAccessToken() = testApplication {
-        val tokenEndpointResponse = getAccessToken()
-        assertEquals(3599, tokenEndpointResponse.expires_in)
-        assertEquals(true, tokenEndpointResponse.access_token.isNotEmpty())
-        assertEquals("Bearer", tokenEndpointResponse.token_type)
     }
 }
