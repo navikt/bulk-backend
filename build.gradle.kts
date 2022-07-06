a2eb14256d982f2b983ea342c69930b49c9525f4
Ulrik Røsby <ulrik@rosby.no>
2022-07-06 09:23:41 +0200

    feat: add dotenv for dynamic loading of environment variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
