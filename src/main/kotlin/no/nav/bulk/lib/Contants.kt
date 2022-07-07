e0e05a7041cd9c3dd2397b172837ac627e37addd
ahmeds99 <ahmed99@live.no>
2022-07-07 14:45:00 +0200

    Revert to version with const in objects
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -7,12 +7,12 @@ val dotenv = dotenv {
 }
 
 object AuthConfig {
-    val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
+    const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
     val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
     val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
 }
 
 object Endpoints {
-    val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
+    const val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
     val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
 }
