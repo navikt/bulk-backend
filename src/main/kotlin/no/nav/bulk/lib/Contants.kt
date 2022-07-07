71fdcad991d6df4e8e77cf8f8bce47ce99b5a185
ahmeds99 <ahmed99@live.no>
2022-07-07 14:52:53 +0200

    Change object to class with companion object
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -6,13 +6,17 @@ val dotenv = dotenv {
     ignoreIfMissing = true
 }
 
-object AuthConfig {
-    const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
-    val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
-    val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
+class AuthConfig {
+    companion object {
+        val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
+        val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
+        val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
+    }
 }
 
-object Endpoints {
-    const val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
-    val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
+class Endpoints {
+    companion object {
+        val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
+        val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
+    }
 }
