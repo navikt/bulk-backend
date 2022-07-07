85fbaf714f8a3ff93b565c3df13fc0c1cf153c08
ahmeds99 <ahmed99@live.no>
2022-07-07 14:39:03 +0200

    fix: removed const from objects
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -7,12 +7,12 @@ val dotenv = dotenv {
 }
 
 object AuthConfig {
-    const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
+    val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
     val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
     val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
 }
 
 object Endpoints {
-    const val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
+    val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
     val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
 }
