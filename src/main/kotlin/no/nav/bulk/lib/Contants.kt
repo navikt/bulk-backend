8488724de28d737f07ed2d8b59e542a45f0edba0
ahmeds99 <ahmed99@live.no>
2022-07-07 14:56:11 +0200

    Revert back to previous object; from class with companion object
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -6,17 +6,13 @@ val dotenv = dotenv {
     ignoreIfMissing = true
 }
 
-class AuthConfig {
-    companion object {
-        val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
-        val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
-        val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
-    }
+object AuthConfig {
+    const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
+    val CLIENT_ID = dotenv["AZURE_APP_CLIENT_ID"]!!
+    val CLIENT_SECRET = dotenv["AZURE_APP_CLIENT_SECRET"]!!
 }
 
-class Endpoints {
-    companion object {
-        val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
-        val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
-    }
+object Endpoints {
+    const val DIGDIR_KRR_API_URL = "https://digdir-krr-proxy.dev.intern.nav.no/rest/v1/personer"
+    val TOKEN_ENDPOINT = dotenv["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"]!!
 }
