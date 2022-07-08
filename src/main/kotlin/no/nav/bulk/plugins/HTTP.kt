a0058c4164c5ef838a44f12740d944bd447b5c45
Ulrik Røsby <ulrik@rosby.no>
2022-07-08 12:57:17 +0200

    fix: add prod url to allowHost
    Co-authored-by: ahmeds99 <ahmed99@live.no>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/main/kotlin/no/nav/bulk/plugins/HTTP.kt
+++ src/main/kotlin/no/nav/bulk/plugins/HTTP.kt
@@ -14,7 +14,7 @@ fun Application.configureHTTP() {
         allowHeadersPrefixed("Nav-")
         // anyHost()
         allowHost("localhost:3000")
-        allowHost("bulk.dev.intern.nav.no")
+        allowHost("https://bulk.dev.intern.nav.no")
     }
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
