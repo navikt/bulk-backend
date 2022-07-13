42ac8e9f47ed209cf29b0db20fd731d2c2208321
Sigbjørn Berdal <sigbjorn.berdal@nav.no>
2022-07-13 14:24:30 +0200

    feat: azuread configuration class
    Co-authored-by: ahmeds99 <ahmed99@live.no>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/models/TokenEndpointResponse.kt
+++ src/main/kotlin/no/nav/bulk/models/TokenEndpointResponse.kt
@@ -1,9 +1,27 @@
 package no.nav.bulk.models
 
-@kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.jsonObject
+
+@Serializable
 data class TokenEndpointResponse(
     val token_type: String,
     val expires_in: Int,
     val ext_expires_in: Int,
     val access_token: String
+)
+
+@Serializable
+data class AzureAdOpenIdConfiguration(
+    @SerialName("jwks_uri")
+    val jwksUri: String,
+    @SerialName("issuer")
+    val issuer: String,
+    @SerialName("token_endpoint")
+    val tokenEndpoint: String,
+    @SerialName("authorization_endpoint")
+    val authorizationEndpoint: String
 )
