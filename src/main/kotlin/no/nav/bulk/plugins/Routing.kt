aa6e472179b230bef26fbef5e64acc99c47bcdf2
ahmeds99 <ahmed99@live.no>
2022-07-19 15:26:52 +0200

    fix: refactor and remove unnecessary authentication from isalive route
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/plugins/Routing.kt
+++ src/main/kotlin/no/nav/bulk/plugins/Routing.kt
@@ -1,9 +1,5 @@
 package no.nav.bulk.plugins
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
-import com.auth0.jwt.interfaces.DecodedJWT
-import com.auth0.jwt.interfaces.RSAKeyProvider
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -11,19 +7,17 @@ import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import no.nav.bulk.lib.*
+import no.nav.bulk.lib.filterAndMapDigDirResponse
+import no.nav.bulk.lib.getAccessToken
+import no.nav.bulk.lib.getContactInfo
 import no.nav.bulk.logger
 import no.nav.bulk.models.PeopleDataRequest
 
 fun Application.configureRouting() {
 
     routing {
-        authenticate {
-            get("/isalive") {
-                val wonderwallToken = this.context.getWonderwallAccessToken()
-                logger.info("Authorization: Bearer $wonderwallToken")
-                call.respond("Alive" + "\n${wonderwallToken}")
-            }
+        get("/isalive") {
+            call.respond("Alive")
         }
 
         get("/isready") {
@@ -32,25 +26,15 @@ fun Application.configureRouting() {
 
         authenticate {
             post("/personer") {
-                // get authorization header
-                val wonderWallccessToken = context.getWonderwallAccessToken()
-                val tokenWithoutBearer = wonderWallccessToken.split(" ")[1]
-                // parse token
-                val jwtDecodedTokenPreviousAud: DecodedJWT = JWT.decode(tokenWithoutBearer)
-
-                val test = generateJwt(jwtDecodedTokenPreviousAud)
-
-                val assertion = jwtDecodedTokenPreviousAud.token
-
-
-                // request now token scoped to the downstream api resource
-                val tokenEndpointResponse = getAccessTokenOBO(assertion)
+                // initate client credentials grant
+                val tokenEndpointResponse = getAccessToken()
 
                 // return if invalid
                 if (tokenEndpointResponse == null) {
                     call.respond(HttpStatusCode.Unauthorized)
                     return@post
                 }
+
                 lateinit var requestData: PeopleDataRequest
                 try {
                     requestData = call.receive()
@@ -89,6 +73,3 @@ fun Application.configureRouting() {
         }
     }
 }
-
-
-fun ApplicationCall.getWonderwallAccessToken(): String = request.parseAuthorizationHeader().toString()
