953d0e086a1d5028be0cd8ce38e522c1f5e9d749
Sigbjørn Berdal <sigbjorn.berdal@nav.no>
2022-07-25 11:22:39 +0200

    fix: map to csv with list.map method and stringbuilder

--- src/main/kotlin/no/nav/bulk/lib/CSV.kt
+++ src/main/kotlin/no/nav/bulk/lib/CSV.kt
@@ -2,22 +2,36 @@ package no.nav.bulk.lib
 
 import no.nav.bulk.logger
 import no.nav.bulk.models.PeopleDataResponse
-import java.time.LocalDateTime
 
 fun mapToCSV(peopleData: PeopleDataResponse): String {
-    val result = StringBuilder()
-    result.append("Personident,Språk,E-post,Mobilnummer,Adresse,Feil")
-    logger.info("Mapping people request to CSV file (string)")
-    logger.info("Time start mapping to CSV: ${LocalDateTime.now()}")
     // With 200k pnrs, this processing can take 7 minutes!!!
-    peopleData.personer.forEach { (personident, personData) ->
-        result.append("\n$personident," +
-                (personData.person?.spraak ?: "") + "," +
-                (personData.person?.epostadresse ?: "") + "," +
-                (personData.person?.mobiltelefonnummer ?: "") + "," +
-                (personData.person?.adresse ?: "") + "," +
-                (personData.feil ?: ""))
+    return peopleData.personer.map { (personident, personData) ->
+        var p = StringBuilder()
+        p.append(personident)
+        p.append(',')
+        p.append(personData.person?.spraak ?: "")
+        p.append(',')
+        p.append(personData.person?.epostadresse ?: "")
+        p.append(',')
+        p.append(personData.person?.mobiltelefonnummer ?: "")
+        p.append(',')
+        p.append(personData.person?.adresse ?: "")
+        p.append(',')
+        p.append(personData.feil ?: "")
+        p.toString()
+    }.joinToString(separator = "\n").also {
+        it.replace("/^/","Personident,Språk,E-post,Mobilnummer,Adresse,Feil\n")
     }
-    logger.info("Time end mapping to CSV: ${LocalDateTime.now()}")
-    return result.toString()
+    //    val result = StringBuilder()
+    //    result.append("Personident,Språk,E-post,Mobilnummer,Adresse,Feil")
+    //    // With 200k pnrs, this processing can take 7 minutes!!!
+    //    peopleData.personer.forEach { (personident, personData) ->
+    //        result.append("\n$personident," +
+    //                (personData.person?.spraak ?: "") + "," +
+    //                (personData.person?.epostadresse ?: "") + "," +
+    //                (personData.person?.mobiltelefonnummer ?: "") + "," +
+    //                (personData.person?.adresse ?: "") + "," +
+    //                (personData.feil ?: ""))
+    //    }
+    //    return result.toString()
 }
