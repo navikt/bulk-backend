ff2ac8ed02bd5710af6bfcd42d3573891ace61f3
ahmeds99 <ahmed99@live.no>
2022-07-25 09:38:17 +0200

    fix: stringbuilder instead of string concatenation
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/lib/CSV.kt
+++ src/main/kotlin/no/nav/bulk/lib/CSV.kt
@@ -1,20 +1,23 @@
+package no.nav.bulk.lib
+
 import no.nav.bulk.logger
 import no.nav.bulk.models.PeopleDataResponse
 import java.time.LocalDateTime
 
 fun mapToCSV(peopleData: PeopleDataResponse): String {
-    var result = "Personident,Språk,E-post,Mobilnummer,Adresse,Feil"
+    val result = StringBuilder()
+    result.append("Personident,Språk,E-post,Mobilnummer,Adresse,Feil")
     logger.info("Mapping people request to CSV file (string)")
     logger.info("Time start mapping to CSV: ${LocalDateTime.now()}")
     // With 200k pnrs, this processing can take 7 minutes!!!
     peopleData.personer.forEach { (personident, personData) ->
-        result += "\n$personident," +
+        result.append("\n$personident," +
                 (personData.person?.spraak ?: "") + "," +
                 (personData.person?.epostadresse ?: "") + "," +
                 (personData.person?.mobiltelefonnummer ?: "") + "," +
                 (personData.person?.adresse ?: "") + "," +
-                (personData.feil ?: "")
+                (personData.feil ?: ""))
     }
     logger.info("Time end mapping to CSV: ${LocalDateTime.now()}")
-    return result
+    return result.toString()
 }
