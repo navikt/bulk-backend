7c22ab0710fc49da03bfba682b77ec92d0e9b9bd
ahmeds99 <ahmed99@live.no>
2022-07-25 10:16:48 +0200

    fix: try-catch call.receive
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>
    Co-authored-by: ulrik2204 <ulrik@rosby.no>

--- src/main/kotlin/no/nav/bulk/plugins/Routing.kt
+++ src/main/kotlin/no/nav/bulk/plugins/Routing.kt
@@ -3,6 +3,7 @@ package no.nav.bulk.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
