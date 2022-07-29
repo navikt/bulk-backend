90f9c9e7f7779c80257c2c683ba6a26e77707fc0
Sigbjørn Berdal <sigbjorn.berdal@nav.no>
2022-07-29 13:44:13 +0200

    fix: test dont break

--- src/test/kotlin/no/nav/bulk/integrationtests/AuthTest.kt
+++ src/test/kotlin/no/nav/bulk/integrationtests/AuthTest.kt
@@ -12,34 +12,6 @@ class AuthTest() {
     private val server: MockOAuth2Server = MockOAuth2Server()
     private val issuerId: String = "966ac572-f5b7-4bbe-aa88-c76419c0f851"
 
-    init {
-        server.start()
-        // Can be anything you choose - should uniquely identify your issuer if you have several
-
-        // Discovery url to authorization server metadata
-        val wellKnownUrl = server.wellKnownUrl(issuerId).toString()
-
-        // Setup your app with metadata from wellKnownUrl and do your testing here
-        val deferredServer: Deferred<NettyApplicationEngine> = runBlocking {
-            coroutineScope {
-                println("server is running")
-                async {
-                    main(arrayOf("--issuerId", issuerId, "--wellKnownUrl", wellKnownUrl))
-                }
-            }
-
-        }
-
-        val engine = main(arrayOf("--authConfig", wellKnownUrl))
-        engine.stop()
-        // ......
-
-        server.shutdown()
-        runBlocking {
-            deferredServer.await().stop()
-        }
-    }
-
     fun `Do some random stuff with auth`() {
         withMockOAuth2Server {
             // Do some stuff with auth
