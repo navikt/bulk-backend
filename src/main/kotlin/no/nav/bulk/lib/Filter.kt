e320055868d17caa4d1acdc98922f0cfa6aea714
Ulrik Røsby <ulrik@rosby.no>
2022-08-01 15:26:20 +0200

    refactor: isValidContactInfo to return DigDirPersonValidationResult insteadd of ContactInfoStatus

--- src/main/kotlin/no/nav/bulk/lib/Filter.kt
+++ src/main/kotlin/no/nav/bulk/lib/Filter.kt
@@ -5,16 +5,8 @@ import java.time.ZonedDateTime
 
 enum class SuccessCaveat {
     NONE,
-    OUTDATED_EMAIL,
-    OUTDATED_PHONE
-}
-
-enum class ContactInfoStatus {
-    BOTH_INVALID,
     INVALID_EMAIL,
-    INVALID_PHONE,
-    NO_CONTACT_INFO,
-    OK
+    INVALID_PHONE
 }
 
 sealed class DigDirPersonValidationResult {
@@ -28,18 +20,18 @@ fun isValidUpdatedDate(dateContactInfoUpdated: String?): Boolean {
     return now.minusMonths(18).isBefore(date)
 }
 
-fun isValidUpdatedDate(personInfo: DigDirPerson): ContactInfoStatus {
+fun isValidContactInfo(personInfo: DigDirPerson): DigDirPersonValidationResult {
     val oudatedEmail = !isValidUpdatedDate(personInfo.epostadresseOppdatert)
     val outdatedPhone = !isValidUpdatedDate(personInfo.mobiltelefonnummerOppdatert)
     val nullEmail = personInfo.epostadresse == null
     val nullPhone = personInfo.mobiltelefonnummer == null
-    if (oudatedEmail && outdatedPhone) return ContactInfoStatus.BOTH_INVALID
-    if (oudatedEmail && nullPhone) return ContactInfoStatus.BOTH_INVALID
-    if (outdatedPhone && nullEmail) return ContactInfoStatus.BOTH_INVALID
-    if (nullEmail && nullPhone) return ContactInfoStatus.NO_CONTACT_INFO
-    if (oudatedEmail || nullEmail) return ContactInfoStatus.INVALID_EMAIL
-    if (outdatedPhone || nullPhone) return ContactInfoStatus.INVALID_PHONE
-    return ContactInfoStatus.OK
+    if (oudatedEmail && outdatedPhone) return DigDirPersonValidationResult.Fail(FeilType.UTDATERT_KONTAKTINFORMASJON)
+    if (oudatedEmail && nullPhone) return DigDirPersonValidationResult.Fail(FeilType.UTDATERT_KONTAKTINFORMASJON)
+    if (outdatedPhone && nullEmail) return DigDirPersonValidationResult.Fail(FeilType.UTDATERT_KONTAKTINFORMASJON)
+    if (nullEmail && nullPhone) return DigDirPersonValidationResult.Fail(FeilType.KAN_IKKE_VARSLES)
+    if (oudatedEmail || nullEmail) return DigDirPersonValidationResult.Success(SuccessCaveat.INVALID_EMAIL)
+    if (outdatedPhone || nullPhone) return DigDirPersonValidationResult.Success(SuccessCaveat.INVALID_PHONE)
+    return DigDirPersonValidationResult.Success(SuccessCaveat.NONE)
 }
 
 fun validateDigDirPersonInfo(personInfo: DigDirPerson): DigDirPersonValidationResult {
@@ -47,14 +39,7 @@ fun validateDigDirPersonInfo(personInfo: DigDirPerson): DigDirPersonValidationRe
     if (personInfo.kanVarsles != true || personInfo.reservert != false || personInfo.aktiv != true) {
         return DigDirPersonValidationResult.Fail(FeilType.KAN_IKKE_VARSLES)
     }
-    // check contact info (email and phone)
-    return when (isValidUpdatedDate(personInfo)) {
-        ContactInfoStatus.OK -> DigDirPersonValidationResult.Success(SuccessCaveat.NONE)
-        ContactInfoStatus.BOTH_INVALID -> DigDirPersonValidationResult.Fail(FeilType.UTDATERT_KONTAKTINFORMASJON)
-        ContactInfoStatus.INVALID_EMAIL -> DigDirPersonValidationResult.Success(SuccessCaveat.OUTDATED_EMAIL)
-        ContactInfoStatus.INVALID_PHONE -> DigDirPersonValidationResult.Success(SuccessCaveat.OUTDATED_PHONE)
-        ContactInfoStatus.NO_CONTACT_INFO -> DigDirPersonValidationResult.Fail(FeilType.KAN_IKKE_VARSLES)
-    }
+    return isValidContactInfo(personInfo)
 }
 
 fun mapToPersonData(personInfo: DigDirPerson, caveat: SuccessCaveat): PersonData {
@@ -62,8 +47,8 @@ fun mapToPersonData(personInfo: DigDirPerson, caveat: SuccessCaveat): PersonData
         Person(
             personident = personInfo.personident,
             spraak = personInfo.spraak,
-            epostadresse = if (caveat == SuccessCaveat.OUTDATED_EMAIL) null else personInfo.epostadresse,
-            mobiltelefonnummer = if (caveat == SuccessCaveat.OUTDATED_PHONE) null else personInfo.mobiltelefonnummer,
+            epostadresse = if (caveat == SuccessCaveat.INVALID_EMAIL) null else personInfo.epostadresse,
+            mobiltelefonnummer = if (caveat == SuccessCaveat.INVALID_PHONE) null else personInfo.mobiltelefonnummer,
             adresse = personInfo.sikkerDigitalPostkasse?.adresse,
             leverandoerAdresse = personInfo.sikkerDigitalPostkasse?.leverandoerAdresse,
             leverandoerSertifikat = personInfo.sikkerDigitalPostkasse?.leverandoerSertifikat
