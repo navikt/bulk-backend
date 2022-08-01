3b73bc3829ab72c127b93bde8bf9acb827ecb1a0
Ulrik Røsby <ulrik@rosby.no>
2022-08-01 15:33:55 +0200

    refactor: remove unused constants

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -20,8 +20,6 @@ fun RunEnv.isProduction() = ENV == "production"
 
 object AuthConfig {
     const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
-    const val TEAM_BULK_GROUP_ID_DEV = "0242dce3-f722-4c6b-ac97-2dd7cc798c4e"
-    const val TEAM_BULK_GROUP_ID_PROD = "e08a856f-6e64-48b0-978b-5b201760fa13"
     val CLIENT_ID: String = dotenv["AZURE_APP_CLIENT_ID"]
     val CLIENT_JWK: String = dotenv["AZURE_APP_JWK"]
     private val AZURE_APP_WELL_KNOWN_URL: String = dotenv["AZURE_APP_WELL_KNOWN_URL"]
