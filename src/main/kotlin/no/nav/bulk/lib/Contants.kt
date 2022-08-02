7c2155ea180c8d0ae86cc986c0d9c3f5f0f00b9c
Sigbjørn Berdal <sigbjorn.berdal@nav.no>
2022-08-02 10:42:53 +0200

    fix: add TEAM_BULK_GROUP_ID_PROD and TEAM_BULK_GROUP_ID_DEV

--- src/main/kotlin/no/nav/bulk/lib/Contants.kt
+++ src/main/kotlin/no/nav/bulk/lib/Contants.kt
@@ -1,10 +1,6 @@
 package no.nav.bulk.lib
 
 import io.github.cdimascio.dotenv.dotenv
-import io.ktor.client.call.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.runBlocking
-import no.nav.bulk.client
 import no.nav.bulk.models.AzureAdOpenIdConfiguration
 import no.nav.bulk.models.buildAzureADConfig
 
@@ -21,6 +17,8 @@ fun RunEnv.isProduction() = ENV == "production"
 
 object AuthConfig {
     const val SCOPE = "api://dev-gcp.team-rocket.digdir-krr-proxy/.default"
+    const val TEAM_BULK_GROUP_ID_PROD = "e08a856f-6e64-48b0-978b-5b201760fa13"
+    const val TEAM_BULK_GROUP_ID_DEV = "0242dce3-f722-4c6b-ac97-2dd7cc798c4e"
     val CLIENT_ID: String = dotenv["AZURE_APP_CLIENT_ID"]
     val CLIENT_JWK: String = dotenv["AZURE_APP_JWK"]
     private val AZURE_APP_WELL_KNOWN_URL: String = dotenv["AZURE_APP_WELL_KNOWN_URL"]
